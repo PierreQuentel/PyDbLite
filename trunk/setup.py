8626e871833cca2f1c8bf7df9417637090da6939
quentel <quentel@9d04e388-db6c-3e47-9caf-5e862389e08b>
2009-05-18 12:32:36 +0000

    Documentation updates

--- trunk/setup.py
+++ trunk/setup.py
@@ -3,11 +3,19 @@
 from distutils.core import setup
 
 setup(name='PyDbLite',
-      version='2.3',
-      description='PyDbLite, a pure-Python in-memory database',
+      version='2.4',
+      description='PyDbLite, a fast, pure-Python in-memory database',
       author='Pierre Quentel',
       author_email='quentel.pierre@wanadoo.fr',
       url='http://pydblite.sourceforge.net/',
-      packages = ['PyDbLite']
+      packages=['PyDbLite'],
+      classifiers=[
+          'Development Status :: 5 - Production/Stable',
+          'Intended Audience :: Developers',
+          'Intended Audience :: System Administrators',
+          'License :: OSI Approved :: BSD License',
+          'Programming Language :: Python',
+          'Topic :: Database :: Database Engines/Servers'
+          ]
      )
 
